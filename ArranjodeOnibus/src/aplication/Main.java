e2cc48214eb38fe7c3b1304639b3697318ae0bb0
Rodrigo Dias <92124010+Rodrigopereiradias@users.noreply.github.com>
2024-08-06 22:11:58 -0300

    print passageiros

--- ArranjodeOnibus/src/aplication/Main.java
+++ ArranjodeOnibus/src/aplication/Main.java
@@ -1,10 +1,47 @@
 package aplication;
 
+import java.util.Scanner;
+
+import entity.Onibus;
+import entity.Passageiro;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		//tecladp
+        Scanner Teclado = new Scanner(System.in);
+        Onibus onibus = new Onibus(1);
+       
+        
+        
+        for (int i = 0; i < onibus.getTotalVagas(); i++) {
+        	
+            System.out.println("Banco" + (i + 1));
+            
+            System.out.print("Nome: ");
+            String nome = Teclado.next();
+            
+            System.out.print("CPF: ");
+            String cpf = Teclado.next();
+            cpf = cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+            
+            System.out.print("Idade: ");
+            int idade = Teclado.nextInt();
+            
+            System.out.print("Sexo (M/F): ");
+            
+            boolean sexo = Teclado.next().equalsIgnoreCase("M");
 
-	}
+            Passageiro passageiro = new Passageiro(nome, cpf, idade, sexo);
+            onibus.adicionaPassageiro(passageiro, i);
+        }
 
+        System.out.println("\nPassageiros no ônibus:");
+        onibus.mostraVagas();
+
+        System.out.println("\nInformações detalhadas dos passageiros:");
+        onibus.mostrarInformacoesPassageiros();
+    }
 }
--- ArranjodeOnibus/src/entity/Onibus.java
+++ ArranjodeOnibus/src/entity/Onibus.java
@@ -6,13 +6,13 @@ public class Onibus {
 	private Passageiro[] vagas;
 	private int totalVagas = 46;
 	
-	//corpo
+	// Construtor
 	public Onibus(int totalVagas) {
 		
 		this.totalVagas = totalVagas;
 		this.vagas = new Passageiro[totalVagas];
 	}
-		
+	// Métodos	
 	public boolean adicionaPassageiro(Passageiro passageiro, int vaga) {
 		if (vaga >= 0 && vaga < totalVagas && vagas[vaga] == null) {
 			vagas[vaga] = passageiro;
@@ -22,8 +22,45 @@ public class Onibus {
 		return false;
 	}
 	
+	public void mostraVagas() {
+        for (int i = 0; i < totalVagas; i++) {
+            if (vagas[i] != null) {
+                System.out.println("Vaga " + i + ": " + vagas[i].getNome());
+            } else {
+                System.out.println("Vaga " + i + ": Vazia");
+            }
+        }
+    }
+	
+	 public void mostrarInformacoesPassageiros() {
+	        for (int i = 0; i < totalVagas; i++) {
+	            if (vagas[i] != null) {
+	                Passageiro p = vagas[i];
+	                System.out.println("Vaga " + (i + 1) + ": Nome: " + p.getNome() + ", CPF: " + p.getCpf() + ", Idade: " + p.getIdade() + ", Sexo: " + (p.isSexo() ? "Masculino" : "Feminino"));
+	            }
+	        }
+	    }
 	
 	// gets e sets
+	public Passageiro[] getVagas() {
+		return vagas;
+	}
 
+	public void setVagas(Passageiro[] vagas) {
+		this.vagas = vagas;
+	}
+
+	public int getTotalVagas() {
+		return totalVagas;
+	}
+
+	public void setTotalVagas(int totalVagas) {
+		this.totalVagas = totalVagas;
+	}
+	
+	
+	
+
+	
 	
 }
